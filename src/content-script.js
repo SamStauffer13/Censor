c187b91c89948e507f27a3a5d0c46a7a0441038b
Sam <Sstauffer.13@gmail.com>
2017-03-18 14:50:27 -0500

    Started on click events + oo dom elements

--- src/content-script.js
+++ src/content-script.js
@@ -2,123 +2,156 @@
 // todo impliment UI
 // todo reduce looping in watch algorithm
 // https://chrome.google.com/webstore/detail/censor/nhmdjmcfaiodoofhminppdjdhfifflbf
+// todo on hover events, on hover of icon, on page load of icon, styling 
 
 'use strict'
-
 class Censor {
 
     constructor() {
 
-        this.CensorService = new CensorService();
+        this.elements = new CensorElements();
 
-        this.icon = new CensorIcon();
+        this.service = new CensorService();
 
-        if (this.CensorService.ShouldRunOnPageLoad()) this.EnableCensor(true);
+        if (this.service.ShouldRunOnPageLoad === true) {
 
-        if (chrome.runtime.onMessage) chrome.runtime.onMessage.addListener((request) => this.EnableCensor(request.enableCensor));
+            document.body.appendChild(this.elements.icon);
+
+            this.service.Start();
+        }
+
+        if (chrome.runtime.onMessage) chrome.runtime.onMessage.addListener((request) => this.OnBrowserIconClick(request.enableCensor));
+
+        this.SetupCensorMenu();
     }
 
-    // todo move this behavior into the service?
-    EnableCensor(enableCensor) {
+    OnBrowserIconClick(enableCensor) {
 
         if (enableCensor === true) {
 
-            this.icon.Display(true);
+            document.body.appendChild(this.elements.icon);
 
-            this.CensorService.CensorDom();
+            this.service.Start();
+
+        } else if (enableCensor === false) {
+
+            if (document.body.contains(this.elements.icon)) document.body.removeChild(this.elements.icon);
+
+            if (document.body.contains(this.elements.menu)) document.body.removeChild(this.elements.menu);
+
+            this.service.Stop();
         }
-        else {
+    }
 
-            this.icon.Display(false);
+    // todo refactor into smaller methods?
+    SetupCensorMenu() {
 
-            this.CensorService.UnCensorDom();
+        this.elements.icon.onclick = () => {
+
+            document.body.appendChild(this.elements.menu);
+
+            document.body.removeChild(this.elements.icon);
         }
 
-        this.CensorService.ListenForDomChanges(enableCensor);
+        // this.elements.menu.onclick = () => {
 
-        this.CensorService.UpdateCensorStatus(enableCensor);
+        //     document.body.appendChild(this.elements.icon);
+
+        //     document.body.removeChild(this.elements.menu);            
+        // }
+
+        let previousText = this.elements.messenger.textContent;
+
+        let restoreText = () => this.elements.messenger.textContent = previousText;
+
+        this.elements.saveButton.onblur = restoreText();
+
+        this.elements.nukeButton.onblur = restoreText();
+
+        this.elements.saveButton.onhover = () => {
+
+            previousText = this.elements.messenger.textContent;
+
+            this.elements.messenger.textContent = 'Save?';
+        }
+
+        this.elements.saveButton.onclick = () => {
+
+            let oldWord = this.elements.inputLeft.value;
+
+            let newWord = this.elements.inputRight.value;
+
+            this.service.UpdateDebauchery(oldWord, newWord);
+
+            this.elements.messenger.textContent = 'Updated';
+        }
     }
 }
 
-// todo, how about just adding and removing from dom rather than toggling hidden?
-class CensorIcon {
+class CensorElements {
+
+    // todo measure performace of this method vs others...
     constructor() {
 
-        this.menu = new CensorSettingsMenu();
+        let styles = {
+            icon: 'censor-icon',
+            menu: 'censor-menu',
+            messenger: 'censor-messenger',
+            spanLeft: 'censor-span-left',
+            inputLeft: 'censor-input-left',
+            spanRight: 'censor-span-right',
+            inputRight: 'censor-input-right',
+            saveButton: 'censor-save-button',
+            nukeButton: 'censor-nuke-button'
+        };
 
-        this.element = document.createElement('img');
-        this.element.src = chrome.extension ? chrome.extension.getURL('resources/icon-large.png') : 'resources/icon-large.png';
-        this.element.classList = 'censor-icon-large';
-        
-        this.element.onclick = () => {
+        let temp = document.createElement('div');
 
-            if (this.menu.IsDisplaying()) {
+        temp.innerHTML = `<img class='${styles.icon}' src='${this.getSrc('resources/icon-large.png')}' >
+        <div class='${styles.menu}'>
+            <span class='${styles.messenger}'> What can I help you with? </span>
+            <span class='${styles.spanLeft}'> Replace </span> <input class='${styles.inputLeft}' type='text' value='[Politics]' /><span class='${styles.spanRight}'> With </span> <input  type='text' value='Kittens' class='${styles.inputRight}' />
+            <img class='${styles.saveButton}' src='${this.getSrc('resources/icon-large.png')}' > <img class='${styles.nukeButton}' src='${this.getSrc('resources/icon-large.png')}' >
+        </div>`;
 
-                this.menu.Display(true);
-
-            } else {
-
-                this.menu.Display(false);
-            }
-        }
-
-        this.Display(false);
-        document.body.appendChild(this.element);
+        this.icon = temp.getElementsByClassName(styles.icon)[0];
+        this.menu = temp.getElementsByClassName(styles.menu)[0];
+        this.messenger = temp.getElementsByClassName(styles.messenger)[0];
+        this.inputLeft = temp.getElementsByClassName(styles.inputLeft)[0];
+        this.inputRight = temp.getElementsByClassName(styles.inputRight)[0];
+        this.saveButton = temp.getElementsByClassName(styles.saveButton)[0];
+        this.nuclearButton = temp.getElementsByClassName(styles.nukeButton)[0];
     }
+    getSrc(path) {
 
-    Display(shouldDisplay) {
-
-        if (shouldDisplay === true) {
-
-            this.element.style.display = "";
-
-        } else {
-
-            this.element.style.display = "none";
-
-            this.menu.Display(false);
-        }
+        return chrome.extension ? chrome.extension.getURL(path) : path;
     }
 }
 
-class CensorSettingsMenu {
-    constructor() {
+class CensorElement {
 
-        this.element = document.createElement("div");
-        this.element.style.display = "none";
-        this.element.className = "censor-container";
+    constructor(element) {
 
-        this.input = document.createElement("input");
-        this.input.className = "censor-user-input";
-        this.element.appendChild(this.input);
-
-        document.body.appendChild(this.element);
+        this.element = element;
     }
 
-    IsDisplaying() {
+    Display() {
 
-        return this.element.style.display === "none";
+        if (document.body.contains(this.element)) document.body.appendChild(this.element);
     }
 
-    Display(shouldDisplay) {
+    Remove() {
 
-        this.element.style.display = shouldDisplay === true ? "" : "none";
+        if (document.body.contains(this.element)) document.body.removeChild(this.element);
     }
 
-    RunTutorial() {
-
-        this.PrintOneLetterAtATime("What is your name?", this.element);
-
-        // var secondMessage = () => this.PrintOneLetterAtATime(' click the nuke button to wipe your settings => ', this.element);        
-    }
-
-    PrintOneLetterAtATime(message, htmlElement, charPosition = 0) {
+    Print(message, charPosition = 0) {
 
         if (charPosition >= message.length) return;
 
-        htmlElement.innerHTML += message[charPosition++];
+        this.element.innerHTML += message[charPosition++];
 
-        setTimeout(() => { this.PrintOneLetterAtATime(message, htmlElement, charPosition); }, 120); // recursion (0_0)
+        setTimeout(() => { this.PrintOneLetterAtATime(message, charPosition); }, 120); // recursion (0_0)
     }
 }
 
@@ -128,23 +161,48 @@ class CensorService {
 
         this.CensorDB = new CensorDataAccess();
 
+        this.facebookOffenders = [];
+
         let deboucer = null;
 
         this.teenageMutant = new MutationObserver(mutations => {
 
-            let observer = () => mutations.forEach(mutation => this.CensorDom());
+            let observer = () => mutations.forEach(mutation => this._CensorDom());
 
             clearTimeout(deboucer);
 
             deboucer = setTimeout(() => observer(), 1000);
         });
-
-        this.facebookOffenders = [];
     }
 
-    UpdateCensorStatus(enabled) {
+    Start() {
 
-        this.CensorDB.UpdateCensorStatus(enabled);
+        this._CensorDom();
+
+        this.teenageMutant.observe(document.body, { childList: true });
+
+        this.CensorDB.UpdateCensorStatus(true);
+    }
+
+    Stop() {
+
+        this.teenageMutant.disconnect();
+
+        this.CensorDB.UpdateCensorStatus(false);
+
+        this.facebookOffenders.forEach((offender) => {
+
+            offender.innerHTML = offender.previousContents;
+        });
+    }
+
+    UpdateDebauchery(oldWord, newWord) {
+
+        let debauchery = this.CensorDB.GetDebauchery();
+
+        debauchery[oldWord] = newWord;
+
+        this.CensorDB.UpdateDebauchery(debauchery);
     }
 
     ShouldRunOnPageLoad() {
@@ -152,19 +210,7 @@ class CensorService {
         return this.CensorDB.IsCensorEnabled();
     }
 
-    ListenForDomChanges(enabled = true) {
-
-        if (enabled === true) {
-
-            this.teenageMutant.observe(document.body, { childList: true });
-        }
-        else if (enabled === false) {
-
-            this.teenageMutant.disconnect();
-        }
-    }
-
-    CensorDom(domToParse = document.body) {
+    _CensorDom(domToParse = document.body) {
 
         let ent = document.createTreeWalker(domToParse, NodeFilter.SHOW_TEXT);
 
@@ -180,7 +226,7 @@ class CensorService {
 
                     if (window.location.hostname == 'www.facebook.com') {
 
-                        let post = this.GetOffendingFacebookPost(ent.currentNode)
+                        let post = this._GetOffendingFacebookPost(ent.currentNode)
 
                         if (post !== null) this.facebookOffenders.push(post);
                     }
@@ -206,26 +252,18 @@ class CensorService {
 
             offender.previousContents = offender.innerHTML;
 
-            offender.innerHTML = this.GetRandomKittenGif();
+            offender.innerHTML = this._GetRandomKittenGif();
         });
     }
 
-    UnCensorDom() {
-
-        this.facebookOffenders.forEach((offender) => {
-
-            offender.innerHTML = offender.previousContents;
-        });
-    }
-
-    GetOffendingFacebookPost(offendingNode) {
+    _GetOffendingFacebookPost(offendingNode) {
 
         while ((offendingNode = offendingNode.parentElement) && !offendingNode.classList.contains('userContentWrapper'));
 
         return offendingNode;
     }
 
-    GetRandomKittenGif() {
+    _GetRandomKittenGif() {
 
         return `<img id="censor-kitty-photo" src="https://thecatapi.com/api/images/get?format=src&type=jpg&size=large&category=boxes"/>`
     }
@@ -300,3 +338,131 @@ class CensorDataAccess {
         localStorage.setItem(this.CensorStatusKey, isEnabled);
     }
 }
+
+// todo everthing below this line is depricated
+// class CensorElement {
+
+//     constructor(name, css) {
+
+//         this.element = document.createElement(name);
+
+//         this.element.className = css;
+//     }
+
+//     // todo, rename this...
+//     Display(elementToAppendTo = document.body) {
+
+//         elementToAppendTo.appendChild(this.element);
+//     }
+
+//     Remove(elementToRemoveFrom = document.body) {
+
+//         elementToRemoveFrom.removeChild(this.element);
+//     }
+
+//     PrintOneLetterAtATime(message, charPosition = 0) {
+
+//         if (charPosition >= message.length) return;
+
+//         this.element.innerHTML += message[charPosition++];
+
+//         setTimeout(() => { this.PrintOneLetterAtATime(message, charPosition); }, 120); // recursion (0_0)
+//     }
+// }
+
+// class Censor {
+
+//     constructor() {
+
+//         this.CensorService = new CensorService();
+
+//         this.censor = new CensorOrchestrator();
+
+//         if (this.CensorService.ShouldRunOnPageLoad()) this.EnableCensor(true);
+
+//         if (chrome.runtime.onMessage) chrome.runtime.onMessage.addListener((request) => this.EnableCensor(request.enableCensor));
+//     }
+
+//     // todo move this behavior into the service
+//     EnableCensor(enableCensor) {
+
+//         if (enableCensor === true) {
+
+//             this.censor.icon.Display();
+
+//             this.CensorService.CensorDom();
+//         }
+//         else {
+
+//             this.censor.icon.Remove();
+
+//             this.CensorService.UnCensorDom();
+//         }
+
+//         this.CensorService.ListenForDomChanges(enableCensor);
+
+//         this.CensorService.UpdateCensorStatus(enableCensor);
+//     }
+// }
+
+// class CensorOrchestrator {
+
+//     constructor() {
+
+//         this.menu = new CensorMenu();
+
+//         this.icon = new CensorIcon('censor-icon-on-page');
+
+//         this.icon.element.onclick = () => {
+
+//             this.icon.Remove();
+
+//             this.menu.Display();
+
+//             this.menu.RunTutorial();
+//         };
+
+//         this.menu.element.onclick = () => {
+
+//             this.menu.Remove();
+
+//             this.icon.Display();
+//         }
+//     }
+// }
+
+// // This is over enginered
+// class CensorMenu extends CensorElement {
+
+//     constructor() {
+
+//         super('div', 'censor-container');
+
+//         this.messenger = new CensorElement('span', 'todo');
+
+//         this.messenger.Display(this.element);
+
+//         this.userInput = new CensorElement("input", "censor-user-input");
+
+//         this.userInput.Display(this.element);
+
+//         this.saveButton = new CensorIcon('censor-icon-in-menu');
+
+//         this.saveButton.Display(this.element);
+//     }
+
+//     RunTutorial() {
+
+//         this.messenger.PrintOneLetterAtATime("Welcome, creature...");
+//     }
+// }
+
+// class CensorIcon extends CensorElement {
+
+//     constructor(css) {
+
+//         super('img', css);
+
+//         this.element.src = chrome.extension ? chrome.extension.getURL('resources/icon-large.png') : 'resources/icon-large.png';
+//     }
+// }
